a5bc8c3cee65daf7d3208380a4e3ba4d9ec96f28
aapskarel <aapskarel@gmail.com>
2017-01-17 00:31:42 +0100

    columns, colors, header blocks etc

--- generate-kalender.py
+++ generate-kalender.py
@@ -3,12 +3,17 @@
 # functions:
 # makecolumn, makeleftbody, makerightbody, makeheader, makefooter, makemain
 
+from dateutil.rrule import rrule, DAILY
+from datetime import datetime, date
 import svgwrite
+from dateutil.relativedelta import relativedelta 
 
 class Kalender:
 	linecolor = (0,0,0)
 	textcolor = (0,0,0)
 	linewidth = 1
+	colwidth = 50
+	colhormarginn = 5
 	backgroundcolor1 = (255,255,255)
 	backgroundcolor2 = (0,0,0)
 	daytexttype = "meh-font"
@@ -16,28 +21,122 @@ class Kalender:
 	year = 2017
 	weekendcolor = ()
 	holydaycolor = ()
-	headerpictures = [("name",size),("more-name",size)]
+	headerpictures = [("name", 10),("more-name",10)]
 	footerpictures = []
 	monthtexttype = "font-type"
 	monthtextsize = 10
 	footertexts = ["sometext","anotherline of text"]
 	headertexts = []
-	headerlogo = ("name", size)
+	headerlogo = ("name", 10)
+	weekdaynames = ["Zo","Ma", "Di", "Wo", "Do", "Vr","Za"]
+	monthnames = ["Jan", "Feb", "Mrt", "Apr", "Mei", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"]
+	
+	schoolvacations = [(2,18),(2,19),(2,20),(2,21),(2,22),(2,23),(2,24),(2,25),(2,26),(5,22),(5,23),(5,24),(5,25),(5,26),(5,27),(5,28),(5,29),(5,30)]
+	holydays = []
+
+
 
 	def __init__(self, year=2017):
 		self.year = year
+		self.dwg = svgwrite.Drawing('test.svg', profile='full')
+
+	def months(self, start_year):
+		start = datetime(start_year, 1, 1)
+		end = datetime(start_year+1, 1,1) - relativedelta(days=1)
+		return [(d.month, d.year, d.day) for d in rrule(DAILY, dtstart=start, until=end)]
+
+	def makeittree(self, alist):
+		temp = {}
+		for x in alist:
+
+			damm = date(x[1], x[0], x[2])
+
+			if x[1] not in temp:
+				temp.update({x[1]:{}})
+			
+			if x[0] not in temp[x[1]]:
+				temp[x[1]].update({x[0]:{'days':{}, 'name':int(damm.strftime("%m"))}})
+
+			if x[2] not in temp[x[1]][x[0]]['days']:
+				temp[x[1]][x[0]]['days'].update({x[2]:int(damm.strftime("%w"))})
+
+		return temp
 
 	def makefooter(self):
 		pass
 
-	def makeheader(self):
-		pass
+	def makeheader(self, months):
+		possi = 0
+		counter = 0
+		for monthkey, monthvalue in months.items():
+			if counter % 3 == 0:
+				possi += self.colhormarginn
+			self.makeheadersquare("test", possi)
+
+			counter += 1
+			possi += self.colwidth + self.colhormarginn
+
+	def makeheadersquare(self, text ,possi):
+		headersquare = svgwrite.shapes.Rect((possi, -40), (self.colwidth,30), stroke='red')
+		self.dwg.add(headersquare)
+		headersquare.fill('white', opacity=0.0)
 
 	def makemain(self):
-		pass
+		possi = 0
+		counter = 0
+		for yearkey, yearvalue in self.makeittree( self.months(2018)).items():
 
-	def makecolumn(self):
-		pass
+			text_style = "font-size:%ipx; font-family:%s" % (10, "Courier New") 
+		 	self.dwg.add(self.dwg.text(yearkey , insert=(0, -10), fill="rgb(255,0,255)", style=text_style))
+
+			for monthkey, monthvalue in yearvalue.items():
+				if counter % 3 == 0:
+					possi += self.colhormarginn
+				self.makecolumn(monthvalue['name'], monthvalue['days'], possi)
+				counter += 1
+				possi += self.colwidth + self.colhormarginn
+			
+			self.makeheader(yearvalue)
+
+
+		self.dwg.save()
+
+		
+
+	def makecolumn(self, monthindex, days, position):
+
+
+		text_style = "font-size:%ipx; font-family:%s" % (5, "Courier New") 
+
+		self.dwg.add(self.dwg.text(self.monthnames[monthindex-1], insert=(position, -3), fill="rgb(255,0,255)", style=text_style))
+		
+		possi = 0
+		for dayskey, daysvalue in days.items():
+
+			possi += 10
+
+			# self.dwg.add(self.dwg.line((position, possi),(self.colwidth + position, possi), stroke='red'))
+			
+			if (daysvalue in [0,6]):
+				self.dwg.add(svgwrite.shapes.Rect((position, possi), (self.colwidth,10), fill='rgb(100,255,100)'))
+			
+			if (monthindex, dayskey) in self.schoolvacations:
+				self.dwg.add(svgwrite.shapes.Rect((position, possi), (self.colwidth/5,10), fill='rgb(255,100,100)'))
+
+			
+
+			text_style = "font-size:%ipx; font-family:%s" % (4, "Courier New") 
+			self.dwg.add(self.dwg.text(self.weekdaynames[daysvalue] + " " + str(dayskey) , insert=(position + 3, possi - 3), fill="rgb(255,0,255)", style=text_style))
+		 	
+ 			somecolumn = svgwrite.shapes.Rect((position, 0), (self.colwidth,320), stroke='red')
+			self.dwg.add(somecolumn)
+			somecolumn.fill('white', opacity=0.0)
+
+		possi = 0
+		for dayskey, daysvalue in days.items():
+			possi += 10
+			self.dwg.add(self.dwg.line((position, possi),(self.colwidth + position, possi), stroke='red'))
+
 
 	def makeother(self):
 		pass
@@ -51,7 +150,7 @@ class Kalender:
 	def setmargins(self,width, height):
 		pass
 
-	def setfootertext(self, texts, size, color)
+	def setfootertext(self, texts, size, color):
 		pass
 
 	def addfootertext(self, texttoadd):
@@ -60,10 +159,10 @@ class Kalender:
 	def setheadertext(self, texts, size, color):
 		pass	
 
-	def addheadertext(self, texts);
+	def addheadertext(self, texts):
 		pass
 
-	def settextcolor(self, color);
+	def settextcolor(self, color):
 		pass
 
 	def sethearpicture(self, pictures, maxsize):
@@ -75,9 +174,12 @@ class Kalender:
 	def setfooterpictures(self, pictures, maxsize):
 		pass
 
-	def addfooterpictures():
+	def addfooterpictures(self):
 		pass
 
 	def addlogo(self, picture, maxsize):
 		pass
 
+if __name__ == "__main__":
+	kalendar = Kalender()
+	kalendar.makemain()
--- test-itterate-days.py
+++ test-itterate-days.py
@@ -1,12 +1,35 @@
 #!/usr/bin/python
 
 from dateutil.rrule import rrule, DAILY
-from datetime import datetime
+from datetime import datetime, date
+import pprint
+
+
+
+pp = pprint.PrettyPrinter(depth=6)
 
 def months(start_month, start_year, end_month, end_year):
     start = datetime(start_year, start_month, 1)
     end = datetime(end_year, end_month, 1)
     return [(d.month, d.year, d.day) for d in rrule(DAILY, dtstart=start, until=end)]
 
+def makeittree(alist):
+	temp = {}
+	for x in alist:
 
-print months(2, 2008, 2 , 2011)
+		damm = date(x[1], x[0], x[2])
+
+		if x[1] not in temp:
+			temp.update({x[1]:{}})
+		
+		if x[0] not in temp[x[1]]:
+			temp[x[1]].update({x[0]:{'days':None, 'name':damm.strftime("%B")}})
+
+		if x[2] not in temp[x[1]][x[0]]:
+			temp[x[1]][x[0]].update({x[2]:damm.strftime("%A")})
+
+	return temp
+
+
+pp.pprint( makeittree( months(2, 2008, 2 , 2011)))
+
